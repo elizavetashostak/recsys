2baaf9eeb34c89829aefbb63267b8349bd79902c
Elizaveta_Masharina <Elizaveta_Masharina@epam.com>
2022-01-10 12:41:25 +0300

    task 3 edited

--- Task_3/test/hello_word_input.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask import request
-app = Flask(__name__)
-
-@app.route('/predict', methods=['GET'])
-def hello_world_input():
-    data = request.query_string
-    return 'Hello, World!' + str(data)
